3d1d24eab9bc26f8e04683d85b79bc2b454e9f71
elmerastudillo <astudilloelmer@icloud.com>
2017-11-17 00:59:16 -0800

    Implemented sorting algorithms

--- source/sorting.py
+++ source/sorting.py
@@ -1,9 +1,15 @@
 #!python
-
+import pdb
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order."""
     # TODO: Check that all adjacent items are in order, return early if not
+    for i in range(len(items) - 1):
+        if items[i] > items[i + 1]:
+            continue
+        else:
+            return False
+    return True
 
 
 def bubble_sort(items):
@@ -11,6 +17,17 @@ def bubble_sort(items):
     repeating until all items are in sorted order."""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
+    # loop through list 
+    for i in range(len(items) - 1):
+        # Loop backwards avoiding the already sorted numbers
+        for j in range(len(items) - 1 - i):
+            # if left item is bigger than the right
+            if items[j] > items[j + 1]:
+                # Swap left and right
+                items[j], items[j + 1] = items[j + 1], items[j]
+    return items
+
+
 
 
 def selection_sort(items):
@@ -19,6 +36,21 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
+    # pdb.set_trace()
+    for i in range(len(items) - 1):
+        # setting the minimum to start with
+        min = i
+        # Start looping from the current index i
+        for j in range(i + 1, len(items)):
+            # if j is less than our current minimum
+            if items[j] < items[min]:
+                # set j to our minimum
+                min = j
+        # Once loop is done set i to be our minimum
+        items[i], items[min] = items[min], items[i]
+    return items
+
+
 
 
 def insertion_sort(items):
@@ -27,9 +59,22 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    for i in range(len(items) - 1):
+        # Loop through the list in reversal until you get to 0
+        for j in range(i - 1, - 1, -1):
+            # If left index is bigger than right index
+            if items[j] > items[j + 1]:
+                # Then swap
+                items[j], items[j + 1] = items[j + 1], items[j]
+            else:
+                # Break once the item is not bigger and insert
+                break
+    return items
+            
 
 
-def test_sorting(sort=bubble_sort, num_items=20, max_value=50):
+
+def test_sorting(sort=selection_sort, num_items=20, max_value=50):
     """Test sorting algorithms with a small list of random items."""
     # Create a list of 8 or 16 items in arbitrary order
     # items = [3, 5, 4, 2, 6, 8, 1, 7]
@@ -43,7 +88,7 @@ def test_sorting(sort=bubble_sort, num_items=20, max_value=50):
     print('Initial items: {!r}'.format(items))
 
     # Change this sort variable to the sorting algorithm you want to test
-    # sort = bubble_sort
+    sort = selection_sort
     print('Sorting items with {}(items)'.format(sort.__name__))
     sort(items)
     print('Sorted items:  {!r}'.format(items))
@@ -84,4 +129,15 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    unsorted_list = [5,4,7,10,1,3,2]
+    # sorted_list = bubble_sort(unsorted_list)
+    # print(sorted_list)
+    sorted_list = selection_sort(unsorted_list)
+    print(sorted_list)
+    sorted_list = insertion_sort(unsorted_list)
+    print(sorted_list)
+    # print(selection_sort(unsorted_list))
+    new_list = bubble_sort(unsorted_list)
+    print(bubble_sort(unsorted_list))
+    # print(new_list)
